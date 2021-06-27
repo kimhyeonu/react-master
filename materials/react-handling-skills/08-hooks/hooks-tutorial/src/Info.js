52565c5a35e9f1231cb2522aa80defc205c48eff
kimhyeonu <polaris@byeolmuri.com>
2021-06-27 16:18:41 +0900

    useEffect

--- materials/react-handling-skills/08-hooks/hooks-tutorial/src/Info.js
+++ materials/react-handling-skills/08-hooks/hooks-tutorial/src/Info.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect()');
+
+    console.log(name);
+
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    };
+  }, []);
 
   const onChangeName = (e) => {
     setName(e.target.value);
