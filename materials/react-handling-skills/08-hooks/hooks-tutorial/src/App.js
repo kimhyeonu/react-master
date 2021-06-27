ae0ce7ceaabd09e0d722df6cea907129d1402aab
kimhyeonu <polaris@byeolmuri.com>
2021-06-27 17:42:59 +0900

    커스텀 Hooks 만들기

--- materials/react-handling-skills/08-hooks/hooks-tutorial/src/App.js
+++ materials/react-handling-skills/08-hooks/hooks-tutorial/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Average from './Average';
+import Info from './Info';
 
 const App = () => {
-  return <Average />;
+  return <Info />;
 };
 
 export default App;
