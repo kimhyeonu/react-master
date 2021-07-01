fef6b85f8b6bd0a3bc72829817160211f2af42c9
kimhyeonu <polaris@byeolmuri.com>
2021-07-01 21:37:30 +0900

    데이터 추가 기능 구현

--- materials/react-handling-skills/10-create-scheduling-web-app/todo-app/src/App.js
+++ materials/react-handling-skills/10-create-scheduling-web-app/todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
@@ -23,9 +23,25 @@ const App = () => {
     },
   ]);
 
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
