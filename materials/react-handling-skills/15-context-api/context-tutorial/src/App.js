9200d1af9498d62f5b23946fd34b9beed4b54704
kimhyeonu <polaris@byeolmuri.com>
2021-07-08 23:32:08 +0900

    색상 변경 기능 구현

--- materials/react-handling-skills/15-context-api/context-tutorial/src/App.js
+++ materials/react-handling-skills/15-context-api/context-tutorial/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { ColorProvider } from './contexts/color';
 import ColorBox from './components/ColorBox';
+import SelectColors from './components/SelectColor';
 
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
