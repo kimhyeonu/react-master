4549fac622580574c61b1479d8e6abea881a31ea
kimhyeonu <polaris@byeolmuri.com>
2021-07-25 22:08:07 +0900

    useContext Hook & static contextType

--- materials/react-handling-skills/15-context-api/context-tutorial/src/components/ColorBox.js
+++ materials/react-handling-skills/15-context-api/context-tutorial/src/components/ColorBox.js
@@ -1,30 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import { ColorConsumer } from '../contexts/color';
+import ColorContext from '../contexts/color';
 
 const ColorBox = () => {
-  return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: '64px',
-              height: '64px',
-              background: state.color,
-            }}
-          />
+  const { state } = useContext(ColorContext);
 
-          <div
-            style={{
-              width: '32px',
-              height: '32px',
-              background: state.subColor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+  return (
+    <>
+      <div
+        style={{
+          width: '64px',
+          height: '64px',
+          background: state.color,
+        }}
+      />
+
+      <div
+        style={{
+          width: '32px',
+          height: '32px',
+          background: state.subColor,
+        }}
+      />
+    </>
   );
 };
 
--- materials/react-handling-skills/15-context-api/context-tutorial/src/components/ColorSelector.js
+++ materials/react-handling-skills/15-context-api/context-tutorial/src/components/ColorSelector.js
@@ -1,40 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { ColorConsumer } from '../contexts/color';
+import ColorContext from '../contexts/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const ColorSelector = () => {
-  return (
-    <div>
-      <h2>색상을 선택하세요.</h2>
+class ColorSelector extends Component {
+  static contextType = ColorContext;
 
-      <ColorConsumer>
-        {({ actions }) => (
-          <div style={{ display: 'flex' }}>
-            {colors.map((color) => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: '24px',
-                  height: '24px',
-                  cursor: 'pointer',
-                }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={(e) => {
-                  e.preventDefault();
-                  actions.setSubColor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
+  handleSetColor = (color) => {
+    this.context.actions.setColor(color);
+  };
 
-      <hr />
-    </div>
-  );
-};
+  handleSetSubColor = (subColor) => {
+    this.context.actions.setSubColor(subColor);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+
+        <div style={{ display: 'flex' }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer',
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetSubColor(color);
+              }}
+            />
+          ))}
+        </div>
+
+        <hr />
+      </div>
+    );
+  }
+}
 
 export default ColorSelector;
