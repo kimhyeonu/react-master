ccc167903a824aa2c89d340f9714c8056d7d58f6
kimhyeonu <polaris@byeolmuri.com>
2021-07-27 20:49:19 +0900

    redux-logger 미들웨어 라이브러리 적용

--- materials/react-handling-skills/18-redux-middleware/learn-redux-middleware/src/index.js
+++ materials/react-handling-skills/18-redux-middleware/learn-redux-middleware/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
 
 import './index.css';
 
 import App from './App';
 import rootReducer from './modules';
+// import loggerMiddleware from './libs/loggerMiddleware';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const logger = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
