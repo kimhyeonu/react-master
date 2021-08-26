35c6e7463ecd18558e72368565eb73b2bca51e75
kimhyeonu <polaris@byeolmuri.com>
2021-08-27 08:24:16 +0900

    redux-actions 적용 : createAction, handleActions

--- react-handling-skills/17-manage-react-app-statue-using-redux/react-redux-tutorial/src/modules/couter.js
+++ react-handling-skills/17-manage-react-app-statue-using-redux/react-redux-tutorial/src/modules/couter.js
@@ -1,14 +1,20 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입
 const DECREASE = 'counter/DECREASE';
 const INCREASE = 'counter/INCREASE';
 
 // 액션 생성 함수
-export const decrease = () => ({
-  type: DECREASE,
-});
-export const increase = () => ({
-  type: INCREASE,
-});
+// [1]
+// export const decrease = () => ({
+//   type: DECREASE,
+// });
+// export const increase = () => ({
+//   type: INCREASE,
+// });
+// [2]
+export const decrease = createAction(DECREASE);
+export const increase = createAction(INCREASE);
 
 // 초기 상태
 const initialState = {
@@ -16,21 +22,30 @@ const initialState = {
 };
 
 // 리듀서
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
+// [1]
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
 
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
 
-    default:
-      return state;
-  }
-}
+//     default:
+//       return state;
+//   }
+// }
+// [2]
+const counter = handleActions(
+  {
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+  },
+  initialState
+);
 
 export default counter;
--- react-handling-skills/17-manage-react-app-statue-using-redux/react-redux-tutorial/src/modules/todoList.js
+++ react-handling-skills/17-manage-react-app-statue-using-redux/react-redux-tutorial/src/modules/todoList.js
@@ -1,3 +1,6 @@
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+
 // 상수
 let lastItemId = 0;
 
@@ -8,26 +11,36 @@ const TOGGLE = 'todoList/TOGGLE';
 const REMOVE = 'todoList/REMOVE';
 
 // 액션 생성 함수
-export const changeInput = (inputText) => ({
-  type: CHANGE_INPUT,
-  inputText,
-});
-export const add = (inputText) => ({
-  type: ADD,
-  item: {
-    id: lastItemId + 1,
-    content: inputText,
-    done: false,
-  },
-});
-export const toggle = (id) => ({
-  type: TOGGLE,
-  id,
-});
-export const remove = (id) => ({
-  type: REMOVE,
-  id,
-});
+// [1]
+// export const changeInput = (inputText) => ({
+//   type: CHANGE_INPUT,
+//   inputText,
+// });
+// export const add = (inputText) => ({
+//   type: ADD,
+//   item: {
+//     id: lastItemId + 1,
+//     content: inputText,
+//     done: false,
+//   },
+// });
+// export const toggle = (id) => ({
+//   type: TOGGLE,
+//   id,
+// });
+// export const remove = (id) => ({
+//   type: REMOVE,
+//   id,
+// });
+// [2]
+export const changeInput = createAction(CHANGE_INPUT, (inputText) => inputText);
+export const add = createAction(ADD, (inputText) => ({
+  id: lastItemId + 1,
+  content: inputText,
+  done: false,
+}));
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
 
 // 초기 상태
 const initialState = {
@@ -37,39 +50,95 @@ const initialState = {
 
 // 리듀서
 // 불변성 유지!
-function todoList(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        inputText: action.inputText,
-      };
+// [1]
+// function todoList(state = initialState, action) {
+//   switch (action.type) {
+//     case CHANGE_INPUT:
+//       return {
+//         ...state,
+//         inputText: action.inputText,
+//       };
 
-    case ADD:
+//     case ADD:
+//       lastItemId++;
+
+//       return {
+//         ...state,
+//         items: state.items.concat(action.item),
+//       };
+
+//     case TOGGLE:
+//       return {
+//         ...state,
+//         items: state.items.map((item) =>
+//           item.id === action.id ? { ...item, done: !item.done } : item
+//         ),
+//       };
+
+//     case REMOVE:
+//       return {
+//         ...state,
+//         items: state.items.filter((item) => item.id !== action.id),
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
+// [2]
+// const todoList = handleActions(
+//   {
+//     [CHANGE_INPUT]: (state, { payload: inputText }) => ({
+//       ...state,
+//       inputText,
+//     }),
+//     [ADD]: (state, { payload: item }) => {
+//       lastItemId++;
+//       return {
+//         ...state,
+//         items: state.items.concat(item),
+//       };
+//     },
+//     [TOGGLE]: (state, { payload: id }) => ({
+//       ...state,
+//       items: state.items.map((item) =>
+//         item.id === id ? { ...item, done: !item.done } : item
+//       ),
+//     }),
+//     [REMOVE]: (state, { payload: id }) => {
+//       return {
+//         ...state,
+//         items: state.items.filter((item) => item.id !== id),
+//       };
+//     },
+//   },
+//   initialState
+// );
+// [3]
+const todoList = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: inputText }) =>
+      produce(state, (draft) => {
+        draft.inputText = inputText;
+      }),
+    [ADD]: (state, { payload: item }) => {
       lastItemId++;
-      return {
-        ...state,
-        items: state.items.concat(action.item),
-      };
-
-    case TOGGLE:
-      return {
-        ...state,
-        items: state.items.map((item) =>
-          item.id === action.id ? { ...item, done: !item.done } : item
-        ),
-      };
-
-    case REMOVE:
-      lastItemId--;
-      return {
-        ...state,
-        items: state.items.filter((item) => item.id !== action.id),
-      };
-
-    default:
-      return state;
-  }
-}
+      return produce(state, (draft) => {
+        draft.items.push(item);
+      });
+    },
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const theItem = draft.items.find((item) => item.id === id);
+        theItem.done = !theItem.done;
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const theIndex = draft.items.findIndex((item) => item.id === id);
+        draft.items.splice(theIndex, 1);
+      }),
+  },
+  initialState
+);
 
 export default todoList;
