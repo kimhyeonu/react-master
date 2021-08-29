b28c4722aeca0539d1fc4426276587710de9ebea
kimhyeonu <polaris@byeolmuri.com>
2021-08-29 21:34:45 +0900

    React.lazy와 Suspense를 통한 컴포넌트 코드 스플리팅

--- react-handling-skills/19-code-splitting/splitting-sample/src/App.js
+++ react-handling-skills/19-code-splitting/splitting-sample/src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import loadable from '@loadable/component';
 
 import logo from './logo.svg';
 import './App.css';
 
+// const SplitMe = React.lazy(() => import('./SplitMe'));
+
+// const SplitMe = loadable(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>Loading...</div>,
+});
+
 function App() {
+  const [visible, setVisible] = useState(false);
+
   const onClick = () => {
-    import('./notify').then((result) => result.default());
+    setVisible(true);
+  };
+
+  const onMouseOver = () => {
+    SplitMe.preload();
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
+
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+
+        {/* <Suspense fallback={<div>Loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense> */}
+
+        {visible && <SplitMe />}
       </header>
     </div>
   );
