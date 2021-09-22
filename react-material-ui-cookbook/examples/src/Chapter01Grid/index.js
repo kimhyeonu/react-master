6ed2b4055b729baafe48ff59a8c29bec7aa73f4b
kimhyeonu <polaris@byeolmuri.com>
2021-09-22 20:43:48 +0900

    2021. 09. 22.

--- react-material-ui-cookbook/examples/src/Chapter01Grid/index.js
+++ react-material-ui-cookbook/examples/src/Chapter01Grid/index.js
@@ -1,9 +1,5 @@
-export {
-  default as UnderstandingBreakpoints
-} from './UnderstandingBreakpoints';
-export {
-  default as AbstractingContainersAndItems
-} from './AbstractingContainersAndItems';
+export { default as UnderstandingBreakpoints } from './UnderstandingBreakpoints';
+export { default as AbstractingContainersAndItems } from './AbstractingContainersAndItems';
 export { default as FixedColumnLayout } from './FixedColumnLayout';
 export { default as ColumnDirection } from './ColumnDirection';
 export { default as FillingSpace } from './FillingSpace';
