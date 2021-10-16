b94c5ffad8e25a2afe2fc9400b56228ecf2004ab
kimhyeonu <polaris@byeolmuri.com>
2021-10-16 14:11:07 +0900

    프로젝트 제거

--- do-it-react-native/chapter-02/c02_01/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export default function App() {
-  const textElement = React.createElement(Text, null, 'Hello world!');
-  return textElement;
-}
