cfae90a572e60274c3e27fea12307db99ca32739
kimhyeonu <polaris@byeolmuri.com>
2021-10-30 12:33:56 +0900

    프로젝트 생성

---
+++ react-native/learn-react-native-first/C04-Styling/react-native-style/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Inline Styling - Text</Text>
+
+      <Text style={styles.error}>Inline Styling - Error</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    padding: 10,
+    fontSize: 26,
+    fontWeight: '600',
+    color: 'black',
+  },
+  error: {
+    padding: 10,
+    fontSize: 26,
+    fontWeight: '400',
+    color: 'red',
+  },
+});
+
+export default App;
