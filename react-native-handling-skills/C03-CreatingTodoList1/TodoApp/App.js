5fd5b5d3ad3dbeaa4750625cd391c4c6874df069
kimhyeonu <polaris@byeolmuri.com>
2021-11-20 00:15:35 +0900

    3.2 TextInput으로 사용자 키보드 입력받기

--- react-native-handling-skills/C03-CreatingTodoList1/TodoApp/App.js
+++ react-native-handling-skills/C03-CreatingTodoList1/TodoApp/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import DateHead from './components/DateHead';
@@ -13,11 +13,16 @@ function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
-        <DateHead date={today} />
+        <KeyboardAvoidingView
+          behavior={Platform.select({ ios: 'padding', android: undefined })}
+          style={styles.avoid}
+        >
+          <DateHead date={today} />
 
-        <EmptyList />
+          <EmptyList />
 
-        <TodoAdder />
+          <TodoAdder />
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -27,6 +32,9 @@ const styles = StyleSheet.create({
   block: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  avoid: {
+    flex: 1,
   },
 });
 
--- react-native-handling-skills/C03-CreatingTodoList1/TodoApp/components/TodoAdder.js
+++ react-native-handling-skills/C03-CreatingTodoList1/TodoApp/components/TodoAdder.js
@@ -1,14 +1,85 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Image,
+  Platform,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Keyboard,
+} from 'react-native';
 
 function TodoAdder() {
-  return <View style={styles.block} />;
+  const [text, setText] = useState('');
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+
+  const button = (
+    <View style={styles.buttonStyle}>
+      <Image source={require('../assets/icons/add-white/add-white.png')} />
+    </View>
+  );
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="할 일을 입력해 주세요."
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        onSubmitEditing={onPress}
+        returnKeyType="done"
+      />
+
+      {Platform.select({
+        ios: (
+          <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+            {button}
+          </TouchableOpacity>
+        ),
+        android: (
+          <View style={styles.circleWrapper}>
+            <TouchableNativeFeedback onPress={onPress}>
+              {button}
+            </TouchableNativeFeedback>
+          </View>
+        ),
+      })}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: 'white',
     height: 64,
-    backgroundColor: 'red',
+    paddingHorizontal: 16,
+    borderColor: '#bdbdbd',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 16,
+  },
+  buttonStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69a',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 24,
   },
 });
 
