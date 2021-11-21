8efc57a41b42fa808f067d27bc6cdc04febe196b
kimhyeonu <polaris@byeolmuri.com>
2021-11-21 22:33:50 +0900

    5.3 다양한 내비게이터

--- react-native-handling-skills/C05-ManagingMultipleScreensWithReactNavigation/LearnReactNavigation/_App_4_MultipleNavigator.js
+++ react-native-handling-skills/C05-ManagingMultipleScreensWithReactNavigation/LearnReactNavigation/_App_4_MultipleNavigator.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import {
-  NavigationContainer,
-  getFocusedRouteNameFromRoute,
-} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import MainScreen from './screens/MainScreen';
 import DetailScreen from './screens/DetailScreen';
 
 const Stack = createNativeStackNavigator();
-
-function getHeaderTitle(route) {
-  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
-  const nameMap = {
-    Home: '홈',
-    Search: '검색',
-    Notification: '알림',
-    Message: '메시지',
-  };
-
-  return nameMap[routeName];
-}
 
 function App() {
   return (
@@ -29,9 +14,7 @@ function App() {
         <Stack.Screen
           name="Main"
           component={MainScreen}
-          options={({ route }) => ({
-            title: getHeaderTitle(route),
-          })}
+          options={{ headerShown: false }}
         />
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
