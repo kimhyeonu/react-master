8ce3b663f44fb747a89d09b8a75a32d9f4407b7c
kimhyeonu <polaris@byeolmuri.com>
2021-11-22 23:25:11 +0900

    프로젝트 준비하기

---
+++ react-native-handling-skills/C06-CreatingDiaryApp1/DayLog/screens/RootStack.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import MainTab from './MainTab';
+import EditorScreen from './EditorScreen';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="MainTab"
+        component={MainTab}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Editor" component={EditorScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default RootStack;
