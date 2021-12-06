de29ad07b1c8bfc92bb23cc562076d30094661a0
kimhyeonu <polaris@byeolmuri.com>
2021-12-06 19:48:26 +0900

    9.3 포스트 작성 기능 구현하기 (진행 중)

---
+++ react-native-handling-skills/C08-CreatingPhotoSharingAppWithFirebase2/PublicGalleryPolaris/components/PostCreatingButton.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Pressable,
+  StyleSheet,
+  Platform,
+  ActionSheetIOS,
+} from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
+
+import PictureUploadingModal from './PictureUploadingModal';
+
+const imagePickerOptions = {
+  mediaType: 'photo',
+  maxWidth: 768,
+  maxHeight: 768,
+  includeBase64: Platform.OS === 'android',
+};
+
+const TAB_BAR_HEIGHT = 49;
+
+function PostCreatingButton() {
+  const insets = useSafeAreaInsets();
+
+  const bottom = Platform.select({
+    ios: TAB_BAR_HEIGHT / 2 + insets.bottom - 4,
+    android: TAB_BAR_HEIGHT / 2,
+  });
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const onPickImage = (res) => {
+    if (res.didCancel || !res) {
+      return;
+    }
+
+    console.log(res);
+  };
+
+  const onLaunchCamera = () => {
+    launchCamera(imagePickerOptions, onPickImage);
+  };
+
+  const onLaunchImageLibrary = () => {
+    launchImageLibrary(imagePickerOptions, onPickImage);
+  };
+
+  const onPress = () => {
+    if (Platform.OS === 'android') {
+      setModalVisible(true);
+      return;
+    }
+
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['카메라로 촬영하기', '사진 선택하기', '취소'],
+        cancelButtonIndex: 2,
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 0) {
+          onLaunchCamera();
+        } else if (buttonIndex === 1) {
+          onLaunchImageLibrary();
+        }
+      }
+    );
+  };
+
+  return (
+    <>
+      <View style={[styles.wrapper, { bottom }]}>
+        <Pressable
+          style={({ pressed }) => [
+            styles.circle,
+            Platform.OS === 'ios' && pressed && { opacity: 0.5 },
+          ]}
+          android_ripple={{ color: '#ffffff' }}
+          onPress={onPress}
+        >
+          <Icon name="camera-alt" color="white" size={24} />
+        </Pressable>
+      </View>
+
+      <PictureUploadingModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        onLaunchCamera={onLaunchCamera}
+        onLaunchImageLibrary={onLaunchImageLibrary}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    position: 'absolute',
+    width: 54,
+    height: 54,
+    zIndex: 5,
+    borderRadius: 27,
+    left: '50%',
+    transform: [{ translateX: -27 }],
+    ...Platform.select({
+      ios: {
+        shadowColor: '#4d4d4d',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+        overflow: 'hidden',
+      },
+    }),
+  },
+  circle: {
+    width: 54,
+    height: 54,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#6200ee',
+    borderRadius: 27,
+  },
+});
+
+export default PostCreatingButton;
--- react-native-handling-skills/C08-CreatingPhotoSharingAppWithFirebase2/PublicGalleryPolaris/screens/MainTab/MainTab.js
+++ react-native-handling-skills/C08-CreatingPhotoSharingAppWithFirebase2/PublicGalleryPolaris/screens/MainTab/MainTab.js
@@ -1,41 +1,56 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import HomeStack from './HomeStack/HomeStack';
 import MyProfileStack from './MyProfileStack/MyProfileStack';
+import PostCreatingButton from '../../components/PostCreatingButton';
 
 const Tab = createBottomTabNavigator();
 
 function MainTab() {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: '#6200ee',
-      }}
-    >
-      <Tab.Screen
-        name="HomeStack"
-        component={HomeStack}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <Icon name="home" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="MyProfileStack"
-        component={MyProfileStack}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <Icon name="person" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <>
+      <View style={styles.block}>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#6200ee',
+          }}
+        >
+          <Tab.Screen
+            name="HomeStack"
+            component={HomeStack}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Icon name="home" size={24} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="MyProfileStack"
+            component={MyProfileStack}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Icon name="person" size={24} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+
+      <PostCreatingButton />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    zIndex: 0,
+  },
+});
 
 export default MainTab;
