2596db47485514ea48bd7d6cd7c74e50820c975a
kimhyeonu <polaris@byeolmuri.com>
2022-01-30 22:11:35 +0900

    05-rendering-lists-conditional-content

---
+++ react-perfect-guide/code/05-rendering-lists-conditional-content/react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
+import ExpensesList from './ExpensesList';
+import './Expenses.css';
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((item) => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+
+        <ExpensesChart expenses={filteredExpenses} />
+
+        <ExpensesList expenses={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
